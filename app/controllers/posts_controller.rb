dd429097af760c53052ac5f69b559bdef9312b5b
Steven Visser <steven_v1987@live.nl>
2021-07-07 16:19:45 +0200

    added association between post and user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,12 +5,12 @@ def index
   @posts = Post.all.order("created_at DESC")
  end
 
- def new
-  @post = Post.new
+def new
+  @post = current_user.posts.build
  end
 
 def create
-  @post = Post.new(post_params)
+  @post = current_user.posts.build(post_params)
 if @post.save
    redirect_to @post
   else
